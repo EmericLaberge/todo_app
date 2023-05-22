a0e9d913a097dd9f386837aa0337721932354c20
Emeric Laberge <emericlaberge@gmail.com>
2023-05-21 20:55:06 -0400

    Stable version

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAtom } from "jotai";
 import {
   todoListAtom,
   inputAtom,
   addTodoAtom,
   removeTodoAtom,
-  checkTodoAtom,
+  editTodoAtom,
+  editInputAtom,
 } from "./atoms";
 import {
   TextField,
@@ -24,15 +25,30 @@ import {
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import CheckIcon from "@mui/icons-material/Check";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import "./TodoApp.css";
-import { Add } from "@mui/icons-material";
 
 const TodoApp = () => {
   const [todoList, setTodoList] = useAtom(todoListAtom);
   const [input, setInput] = useAtom(inputAtom);
   const [, addTodo] = useAtom(addTodoAtom);
   const [, removeTodo] = useAtom(removeTodoAtom);
-  const [checked, setChecked] = useAtom(checkTodoAtom);
+  const [, editTodo] = useAtom(editTodoAtom);
+  const [editInput, setEditInput] = useAtom(editInputAtom);
+  const [editingTodo, setEditingTodo] = useState(null);
+
+  const handleSaveTodo = (id) => {
+    editTodo({ id, newText: editInput });
+    setEditingTodo(null);
+    setEditInput("");
+  };
+
+  const handleEditTodo = (id, text) => {
+    setEditingTodo(id);
+    setEditInput(text);
+  };
 
   const handleAddTodo = () => {
     if (input.trim() !== "") {
@@ -46,59 +62,260 @@ const TodoApp = () => {
   };
 
   const handleCheckTodo = (id) => {
-    setChecked(id);
+    if (editingTodo === id) return;
+    setTodoList((prevTodoList) =>
+      prevTodoList.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
   };
+
+  const handleEditKeyPress = (e, todoId) => {
+    if (e.key === "Enter") {
+      e.preventDefault(); // Prevent form submission
+      handleSaveTodo(todoId);
+    }
+  };
+
+  const handleOnDragEnd = (result) => {
+    const { source, destination } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (source.droppableId !== destination.droppableId) {
+      // dragging between two lists
+      return;
+    }
+
+    if (source.droppableId === "nonCompletedTodos") {
+      const items = Array.from(nonCompletedTodoList);
+      const [reorderedItem] = items.splice(source.index, 1);
+      items.splice(destination.index, 0, reorderedItem);
+      setTodoList([...items, ...completedTodoList]);
+    } else {
+      const items = Array.from(completedTodoList);
+      const [reorderedItem] = items.splice(source.index, 1);
+      items.splice(destination.index, 0, reorderedItem);
+      setTodoList([...nonCompletedTodoList, ...items]);
+    }
+  };
+
+  const nonCompletedTodoList = todoList.filter((todo) => !todo.completed);
+  const completedTodoList = todoList.filter((todo) => todo.completed);
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-      <TextField
-        className="input"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="New Todo..."
-      />
-      <Fab aria-label="add" color="primary" onClick={handleAddTodo}>
-        <AddIcon />
-      </Fab>
-      <List className="list">
-        {todoList
-          .sort((a, b) => a.completed - b.completed)
-          .map((todo) => (
-            <ListItem
-              key={todo.id}
-              secondaryAction={
-                <IconButton
-                  edge="end"
-                  aria-label="delete"
-                  onClick={() => handleRemoveTodo(todo.id)}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              }
+      <Typography variant="h3" textAlign={"center"}>
+        Todo List
+      </Typography>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <TextField
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              handleAddTodo();
+            }
+          }}
+          placeholder="New Todo..."
+        />
+        <IconButton
+          aria-label="add"
+          color="primary"
+          variant="contained"
+          onClick={handleAddTodo}
+        >
+          <AddIcon />
+        </IconButton>
+      </Box>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Typography variant="h4">Non-completed tasks:</Typography>
+        <Droppable droppableId="nonCompletedTodos">
+          {(provided) => (
+            <List
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="list"
             >
-              <ListItemButton
-                role={undefined}
-                onClick={() => handleCheckTodo(todo.id)}
-                dense
-              >
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={todo.completed} // make sure the checkbox is checked if the todo is completed
-                    disableRipple
-                    onClick={() => handleCheckTodo(todo.id)}
-                  />
-                </ListItemIcon>
-                <ListItemText
-                  primary={todo.text}
-                  style={{
-                    textDecoration: todo.completed ? "line-through" : "none",
-                  }}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-      </List>
+              {nonCompletedTodoList.map((todo, index) => (
+                <Draggable
+                  key={todo.id}
+                  draggableId={todo.id.toString()}
+                  index={index}
+                >
+                  {(provided) => (
+                    <ListItem
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {/* The code for non-completed tasks */}
+                      <ListItemButton
+                        role={undefined}
+                        onClick={() => handleCheckTodo(todo.id)}
+                        style={
+                          todo.completed && editingTodo !== todo.id
+                            ? { backgroundColor: "#e0e0e0" }
+                            : { backgroundColor: "#fff" }
+                        }
+                        dense
+                      >
+                        {editingTodo !== todo.id && (
+                          <ListItemIcon>
+                            <Checkbox
+                              edge="start"
+                              checked={todo.completed}
+                              disableRipple
+                            />
+                          </ListItemIcon>
+                        )}
+                        {editingTodo === todo.id ? (
+                          <TextField
+                            value={editInput}
+                            onChange={(e) => setEditInput(e.target.value)}
+                            onKeyPress={(e) => handleEditKeyPress(e, todo.id)}
+                          />
+                        ) : (
+                          <ListItemText
+                            primary={todo.text}
+                            style={{
+                              textDecoration: todo.completed
+                                ? "line-through"
+                                : "none",
+                            }}
+                          />
+                        )}
+                        {editingTodo !== todo.id && (
+                          <IconButton
+                            edge="end"
+                            aria-label="edit"
+                            onClick={() => handleEditTodo(todo.id, todo.text)}
+                          >
+                            <EditIcon />
+                          </IconButton>
+                        )}
+                        {editingTodo !== todo.id && (
+                          <IconButton
+                            edge="end"
+                            aria-label="delete"
+                            onClick={() => handleRemoveTodo(todo.id)}
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                        )}
+                        {editingTodo === todo.id && (
+                          <IconButton
+                            edge="end"
+                            aria-label="save"
+                            onClick={() => handleSaveTodo(todo.id)}
+                          >
+                            <CheckIcon />
+                          </IconButton>
+                        )}
+                      </ListItemButton>
+                    </ListItem>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </List>
+          )}
+        </Droppable>
+        <Typography variant="h4">Completed tasks:</Typography>
+        <Droppable droppableId="completedTodos">
+          {(provided) => (
+            <List
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="list"
+            >
+              {completedTodoList.map((todo, index) => (
+                <Draggable
+                  key={todo.id}
+                  draggableId={todo.id.toString()}
+                  index={index}
+                >
+                  {(provided) => (
+                    <ListItem
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {/* The code for completed tasks */}
+                      <ListItemButton
+                        role={undefined}
+                        onClick={() => handleCheckTodo(todo.id)}
+                        style={
+                          todo.completed && editingTodo !== todo.id
+                            ? { backgroundColor: "#e0e0e0" }
+                            : { backgroundColor: "#fff" }
+                        }
+                        dense
+                      >
+                        {editingTodo !== todo.id && (
+                          <ListItemIcon>
+                            <Checkbox
+                              edge="start"
+                              checked={todo.completed}
+                              disableRipple
+                            />
+                          </ListItemIcon>
+                        )}
+                        {editingTodo === todo.id ? (
+                          <TextField
+                            value={editInput}
+                            onChange={(e) => setEditInput(e.target.value)}
+                            onKeyPress={(e) => handleEditKeyPress(e, todo.id)}
+                          />
+                        ) : (
+                          <ListItemText
+                            primary={todo.text}
+                            style={{
+                              textDecoration: todo.completed
+                                ? "line-through"
+                                : "none",
+                            }}
+                          />
+                        )}
+                        {editingTodo !== todo.id && (
+                          <IconButton
+                            edge="end"
+                            aria-label="edit"
+                            onClick={() => handleEditTodo(todo.id, todo.text)}
+                          >
+                            <EditIcon />
+                          </IconButton>
+                        )}
+                        {editingTodo !== todo.id && (
+                          <IconButton
+                            edge="end"
+                            aria-label="delete"
+                            onClick={() => handleRemoveTodo(todo.id)}
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                        )}
+                        {editingTodo === todo.id && (
+                          <IconButton
+                            edge="end"
+                            aria-label="save"
+                            onClick={() => handleSaveTodo(todo.id)}
+                          >
+                            <CheckIcon />
+                          </IconButton>
+                        )}
+                      </ListItemButton>
+                    </ListItem>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </List>
+          )}
+        </Droppable>
+      </DragDropContext>
     </Box>
   );
 };
--- src/atoms.js
+++ src/atoms.js
@@ -2,37 +2,25 @@ import { atom } from "jotai";
 
 export const todoListAtom = atom([]);
 export const todoIndexAtom = atom(0);
-export const todoCompletedListAtom = atom([]);
 export const inputAtom = atom("");
-
+export const editInputAtom = atom("");
 export const addTodoAtom = atom(null, (get, set, newTodoItem) => {
   const todoList = get(todoListAtom);
   const todoIndex = get(todoIndexAtom);
-
   const newTodo = { id: todoIndex, text: newTodoItem, completed: false };
-  set(todoListAtom, [...todoList, newTodo]);
+  set(todoListAtom, [newTodo, ...todoList]);
   set(todoIndexAtom, todoIndex + 1);
 });
-
 export const removeTodoAtom = atom(null, (get, set, id) => {
   const todoList = get(todoListAtom);
-
-  const filteredTodo = todoList.filter((todo) => {
-    return todo.id !== id;
-  });
-
+  const filteredTodo = todoList.filter((todo) => todo.id !== id);
   set(todoListAtom, filteredTodo);
 });
-export const checkTodoAtom = atom(null, (get, set, todoId) => {
+export const editTodoAtom = atom(null, (get, set, { id, newText }) => {
   const todoList = get(todoListAtom);
-  // Find the todo that needs to be checked
-  const todoIndex = todoList.findIndex((todo) => todo.id === todoId);
+  const todoIndex = todoList.findIndex((todo) => todo.id === id);
   if (todoIndex === -1) return;
-  // Clone the todo, toggle its completed state
-  const updatedTodo = {
-    ...todoList[todoIndex],
-    completed: !todoList[todoIndex].completed,
-  };
+  const updatedTodo = { ...todoList[todoIndex], text: newText };
   const updatedTodoList = [...todoList];
   updatedTodoList[todoIndex] = updatedTodo;
   set(todoListAtom, updatedTodoList);
